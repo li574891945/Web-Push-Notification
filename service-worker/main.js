c653e6d2ed09f56ebd2256ea95ae4af1506c3fcc
lijianjun <574891945@qq.com>
2023-10-17 14:14:33 +0800

    优化

--- service-worker/main.js
+++ service-worker/main.js
@@ -1,106 +1,104 @@
 let subscribeButton = document.getElementById("subscribe");
 let unsubscribeButton = document.getElementById("unsubscribe");
 
-console.log(new Date().getTime())
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-      .register("./sw.js",{
+    navigator.serviceWorker.register("./sw.js",{
         scope: ".", // <--- THIS BIT IS REQUIRED
-      })
-      .then(async function (registration) {
-        console.log("Service Worker registered successfully:", registration);
+    })
+        .then(async function (registration) {
+            console.log("Service Worker registered successfully:", registration);
 
-        subscribeButton.addEventListener("click", function () {
-          subscribeToPushNotifications(registration);
-        });
+            subscribeButton.addEventListener("click", function () {
+                subscribeToPushNotifications(registration);
+            });
 
-        unsubscribeButton.addEventListener("click", function () {
-          unsubscribeFromPushNotifications(registration);
+            unsubscribeButton.addEventListener("click", function () {
+                unsubscribeFromPushNotifications(registration);
+            });
+        })
+        .catch(function (error) {
+            console.log("Service Worker registration failed:", error);
         });
-      })
-      .catch(function (error) {
-        console.log("Service Worker registration failed:", error);
-      });
 }
 
 function subscribeToPushNotifications(registration) {
-  registration.pushManager
-      .subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(
-            "BPMR8M4R8tvhMIBgA6I_P7EJHc5OdxDNNEPfkiuLSwE81f872uoPi7fU678zOWUqR3Ze83kdVhozF8xdeX4ZsCU"
-        ),
-      })
-      .then(function (subscription) {
-        console.log("Subscribed to push notifications:", subscription);
-        updateSubscriptionOnServer(subscription);
-        // updateSubscriptionOnServerToMike(subscription)
-        subscribeButton.disabled = true;
-        unsubscribeButton.disabled = false;
-      })
-      .catch(function (error) {
-        console.log("Failed to subscribe to push notifications:", error);
-        subscribeButton.disabled = false;
-        unsubscribeButton.disabled = true;
-      });
+    registration.pushManager
+        .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array(
+                "BPMR8M4R8tvhMIBgA6I_P7EJHc5OdxDNNEPfkiuLSwE81f872uoPi7fU678zOWUqR3Ze83kdVhozF8xdeX4ZsCU"
+            ),
+        })
+        .then(function (subscription) {
+            console.log("Subscribed to push notifications:", subscription);
+            updateSubscriptionOnServer(subscription);
+            updateSubscriptionOnServerToMike(subscription)
+            subscribeButton.disabled = true;
+            unsubscribeButton.disabled = false;
+        })
+        .catch(function (error) {
+            console.log("Failed to subscribe to push notifications:", error);
+            subscribeButton.disabled = false;
+            unsubscribeButton.disabled = true;
+        });
 }
 
 function unsubscribeFromPushNotifications(registration) {
-  registration.pushManager.getSubscription().then(function (subscription) {
-    if (subscription) {
-      subscription
-          .unsubscribe()
-          .then(function () {
-            console.log("Unsubscribed from push notifications:", subscription);
-            updateSubscriptionOnServer(null);
-            // updateSubscriptionOnServerToMike(null)
-            subscribeButton.disabled = false;
-            unsubscribeButton.disabled = true;
-          })
-          .catch(function (error) {
-            console.log("Failed to unsubscribe from push notifications:", error);
-            subscribeButton.disabled = true;
-            unsubscribeButton.disabled = false;
-          });
-    }
-  });
+    registration.pushManager.getSubscription().then(function (subscription) {
+        if (subscription) {
+            subscription
+                .unsubscribe()
+                .then(function () {
+                    console.log("Unsubscribed from push notifications:", subscription);
+                    updateSubscriptionOnServer(null);
+                    updateSubscriptionOnServerToMike(null)
+                    subscribeButton.disabled = false;
+                    unsubscribeButton.disabled = true;
+                })
+                .catch(function (error) {
+                    console.log("Failed to unsubscribe from push notifications:", error);
+                    subscribeButton.disabled = true;
+                    unsubscribeButton.disabled = false;
+                });
+        }
+    });
 }
 
 async function updateSubscriptionOnServer(subscription) {
-  // TODO: Send subscription to server for storage and use
-  console.log(subscription)
-  const SERVER_URL = "https://jim-api.123998.me/jimapi/save-subscription";
-  const response = await fetch(SERVER_URL, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(subscription)
-  });
-  return response.json();
+    // TODO: Send subscription to server for storage and use
+    console.log(subscription)
+    const SERVER_URL = "https://jim-api.123998.me/jimapi/save-subscription";
+    const response = await fetch(SERVER_URL, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(subscription)
+    });
+    return response.json();
 }
 
 async function updateSubscriptionOnServerToMike(subscription) {
-  // TODO: Send subscription to server for storage and use
+    // TODO: Send subscription to server for storage and use
 
-  const SERVER_URL = "https://lark.semfoundry.com/api/LarkInform/save_subscription";
-  const response = await fetch(SERVER_URL, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(subscription),
-  });
-  return response.json();
+    const SERVER_URL = "https://lark.semfoundry.com/api/LarkInform/save_subscription";
+    const response = await fetch(SERVER_URL, {
+        method: "post",
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: JSON.stringify(subscription),
+    });
+    return response.json();
 }
 
 function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 }
--- service-worker/sw.js
+++ service-worker/sw.js
@@ -2,28 +2,30 @@ let json = {
   'msg-url':''
 }
 
-self.addEventListener("push", function (event) {
+self.addEventListener("push",  (event) => {
   console.log("Received a push message", event);
   console.log(new Date().getTime())
   console.log(event.data.json())
   console.log(event.data.text())
 
   const data = event.data.json()
-  const json = event.data.json()
+  json = event.data.json()
 
   const title = data.title;
   const options = {
     body: data.body,
     icon: data.icon,
+    image: data.icon
   };
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
 
-self.addEventListener("notificationclick", function (event) {
+self.addEventListener("notificationclick",  (event) => {
   console.log("Notification clicked", event);
-  console.log(event.data)
+  console.log(event)
+  console.log(json)
 
-  event.notification.close(json);
+  event.notification.close();
   event.waitUntil(clients.openWindow(json["msg-url"]));
 });
